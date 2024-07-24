1ea95e5b91cb786ff0d576ef4d31b307a3fbd992
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-25 02:05:23 +0300

    H

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
     })
     .catch(() => {
       console.log('Signup system offline');
