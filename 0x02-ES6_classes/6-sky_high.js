a5a58ea26b4a514110c5c80f34d20fbe5f1d5b2e
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-26 19:34:32 +0300

    EXTENDS BUILDING

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,5 +1,5 @@
 import Building from './5-building';
-export default class SkyHighBuilding {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this.floors = floors;
