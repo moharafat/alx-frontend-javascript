590b6c6e29580d706c2b3e3806247127b7582d6a
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-26 14:23:20 +0300

    lint

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
-import ClassRoom from './0-classroom'
-export default function initializeRooms () {
-  const d = new ClassRoom(19)
-  const f = new ClassRoom(20)
-  const g = new ClassRoom(34)
-  return [d, f, g]
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const d = new ClassRoom(19);
+  const f = new ClassRoom(20);
+  const g = new ClassRoom(34);
+  return [d, f, g];
 }
