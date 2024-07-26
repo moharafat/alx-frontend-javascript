6d3e16cc85f7c2468960c0d3aacdbd83b86e79de
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-26 16:28:34 +0300

    s

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from './5-building';
+export default class SkyHighBuilding {
+  constructor(sqft, floors) {
+    this.sqft = (sqft);
+    this.floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
