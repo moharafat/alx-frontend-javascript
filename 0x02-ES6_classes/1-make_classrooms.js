8d31a23242c6fb28f354504df7114dc8d4757b8f
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-26 08:25:42 +0300

    FORGOT THE Rreturn [d, f, g]

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from './0-classroom'
+export default function initializeRooms () {
+  const d = new ClassRoom(19)
+  const f = new ClassRoom(20)
+  const g = new ClassRoom(34)
+  return [d, f, g]
+}
