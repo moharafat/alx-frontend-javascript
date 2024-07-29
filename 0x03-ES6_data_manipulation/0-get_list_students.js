c7585ea06fb6214d3b3c2ddd3ab8907bae0333f6
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-30 00:13:02 +0300

    f

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,7 @@
-function getListStudents() {
-  // Create an array to hold the student objects
-  const students = [
-    // Create each student object with the specified properties
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+export default function getListStudents () {
+  return [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
   ];
-
-  // Return the array
-  return students;
 }
