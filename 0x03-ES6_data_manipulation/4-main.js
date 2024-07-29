5dcac822fedaba2dbf490c4338e3e6d17d313308
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-30 00:05:12 +0300

    task 0 link is down???

---
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -0,0 +1,2 @@
+import getListStudents from "./0-get_list_students.js";
+import updateStudentGradeByCity from "./4-update_grade_by_city.js";
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,2 @@
+# alx-frontend-javascript
+### 0x03. ES6 data manipulation 
