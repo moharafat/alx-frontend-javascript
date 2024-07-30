d2bd8c9e33fd4c1c30b96ffe83ed0e66fb29df9b
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-30 14:25:37 +0300

    lint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,16 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   // Filter students by the specified city
   return students
-    .filter(student => student.location === city)
-    .map(student => {
+    .filter((student) => student.location === city)
+    .map((student) => {
       // Find the grade for the current student
-      const gradeEntry = newGrades.find(grade => grade.studentId === student.id);
+      const gradeEntry = newGrades.find((grade) => grade.studentId === student.id);
       // Return the updated student object with grade
       return {
         id: student.id,
         firstName: student.firstName,
         location: student.location,
-        grade: gradeEntry ? gradeEntry.grade : 'N/A'
+        grade: gradeEntry ? gradeEntry.grade : 'N/A',
       };
     });
 }
