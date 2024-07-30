7b01cbe8492e41af85beeb197bd667a40ab33155
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-30 06:53:20 +0300

    elment

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(stList) {
   if (stList instanceof Array) {
-    return stList.map((student) => student.id);
+    return stList.map((elementStudent) => elementStudent.id);
   }
   return [];
 }
