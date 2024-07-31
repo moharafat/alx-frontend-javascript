45a88abdca69757c912d90a665e6549accf741fd
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-08-01 01:39:26 +0300

    task 1

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,17 @@
+var Mohamed = {
+    firstName: "Mohamed",
+    lastName: "Arafat",
+    age: 27,
+    location: "Egypt"
+};
+var a7ma = {
+    firstName: "A7ma",
+    lastName: "Khalil",
+    age: 25,
+    location: "Egypt"
+};
+var studentList = [Mohamed, a7ma];
+function render_Table() {
+    document.createElement("DIV");
+}
+render_Table();
---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,15 @@
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+// should print
+// Object
+// contract: false
+// firstName: "John"
+// fullTimeEmployee: false
+// lastName: "Doe"
+// location: "London"
